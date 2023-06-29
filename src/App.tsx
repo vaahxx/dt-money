628dff48e76f68822fd2d60cb598ac0548e9e06c
Valentina Garavaglia <vaahxx@gmail.com>
2023-06-29 15:37:29 -0400

    chore: implemented useContext

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { ThemeProvider } from "styled-components";
 import { Transactions } from "./pages/Transactions";
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
+import { TransactionsProvider } from "./contexts/TransactionsContext";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <Transactions />
+      <TransactionsProvider>
+        <Transactions />
+      </TransactionsProvider>
     </ThemeProvider>
   );
 }

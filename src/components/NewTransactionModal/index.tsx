92e2bb308c417d30cf521f9f9d0a22a5c7cd4909
Valentina Garavaglia <vaahxx@gmail.com>
2023-07-22 16:50:12 -0400

    fix: fixed build

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -77,7 +77,9 @@ export function NewTransactionModal() {
             render={({ field }) => {
               return (
                 <TransactionType
-                  onValueChange={field.onChange}
+                  onValueChange={(value: NewTransactionFormInputs["type"]) => {
+                    field.onChange(value)
+                  }}
                   value={field.value}
                 >
                   <TransactionTypeButton variant='income' value='income'>
